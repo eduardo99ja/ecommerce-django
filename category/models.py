5418c09c6647f35c3ed860b3bc4911fac86c2377
Eduardo Apodaca <eduardo1ja99@gmail.com>
2021-05-18 16:22:30 -0500

    feat: Pre-populate category slug

--- category/models.py
+++ category/models.py
@@ -5,7 +5,7 @@ class Category(models.Model):
     """Model definition for Category."""
 
     category_name = models.CharField(unique=True, max_length=50)
-    slug = models.CharField(unique=True, max_length=100)
+    slug = models.SlugField(unique=True, max_length=100)
     description = models.TextField(max_length=255, blank=True)
     cat_image = models.ImageField(upload_to='photos/categories', blank=True)
 
