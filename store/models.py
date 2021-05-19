00ce7995d547258bfa521712586c110f1c071910
Eduardo Apodaca <eduardo1ja99@gmail.com>
2021-05-19 15:53:08 -0500

    feat: Store app and product model

---
+++ store/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+from category.models import Category
+
+# Create your models here.
+
+
+class Product(models.Model):
+    """Model definition for Product."""
+
+    product_name = models.CharField(unique=True, max_length=200)
+    slug = models.SlugField(max_length=200, unique=True)
+    description = models.TextField(max_length=500, blank=True)
+    price = models.IntegerField()
+    images = models.ImageField(upload_to='photos/products')
+    stock = models.IntegerField()
+    is_available = models.BooleanField(default=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    created_date = models.DateTimeField(auto_now_add=True)
+    modified_date = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        """Meta definition for Product."""
+
+        verbose_name = 'Product'
+        verbose_name_plural = 'Products'
+
+    def __str__(self):
+        """Unicode representation of Product."""
+        return self.product_name
