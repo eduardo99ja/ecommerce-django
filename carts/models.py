8bc40d8242e5d0f33268185142e6f1fc0147f9e9
Eduardo Apodaca <eduardo1ja99@gmail.com>
2021-05-21 20:32:18 -0500

    feat: Cart and CartItem models

--- carts/models.py
+++ carts/models.py
@@ -1,3 +1,31 @@
+from store.models import Product
 from django.db import models
 
 # Create your models here.
+
+
+class Cart(models.Model):
+    cart_id = models.CharField(max_length=250, blank=True)
+    date_added = models.DateField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return self.cart_id
+
+
+class CartItem(models.Model):
+    """Model definition for CartItem."""
+
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    quantity = models.IntegerField()
+    is_active = models.BooleanField(default=True)
+
+    class Meta:
+        """Meta definition for CartItem."""
+
+        verbose_name = 'CartItem'
+        verbose_name_plural = 'CartItems'
+
+    def __str__(self):
+        """Unicode representation of CartItem."""
+        return self.product
