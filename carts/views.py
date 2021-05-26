f7193e69414c49dbedb6019fd78acde329f0e395
Eduardo Apodaca <eduardo1ja99@gmail.com>
2021-05-26 12:56:53 -0500

    feat: Add to cart with session key

--- carts/views.py
+++ carts/views.py
@@ -1,6 +1,42 @@
-from django.shortcuts import render
+from django.http.response import HttpResponse
+from django.shortcuts import redirect, render
+from .models import Cart, CartItem, Product
 
 # Create your views here.
+
+
+def _cart_id(request):
+    cart = request.session.session_key
+    if not cart:
+        cart = request.session.create()
+    return cart
+
+
+def add_cart(request, product_id):
+    product = Product.objects.get(id=product_id)
+    try:
+        # get the cart using the cart_id present in session
+        cart = Cart.objects.get(cart_id=_cart_id(request))
+    except Cart.DoesNotExist:
+        cart = Cart.objects.create(
+            cart_id=_cart_id(request)
+        )
+    cart.save()
+
+    try:
+        cart_item = CartItem.objects.get(product=product, cart=cart)
+        cart_item.quantity += 1
+        cart_item.save()
+    except CartItem.DoesNotExist:
+        cart_item = CartItem.objects.create(
+            product=product,
+            quantity=1,
+            cart=cart
+        )
+        cart_item.save()
+    return HttpResponse(cart_item.product)
+    exit()
+    return redirect('cart')
 
 
 def cart(request):
