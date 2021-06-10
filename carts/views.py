f974f72c381795d6ab92baebef8186bd19fd0570
Eduardo Apodaca <eduardo1ja99@gmail.com>
2021-06-10 10:42:34 -0500

    feat: Variation manager for variation model

--- carts/views.py
+++ carts/views.py
@@ -14,10 +14,12 @@ def _cart_id(request):
 
 
 def add_cart(request, product_id):
-    color = request.GET['color']
-    size = request.GET['size']
-    return HttpResponse(color + ' ' + size)
-    exit()
+    if request.method == 'POST':
+        color = request.POST['color']
+        size = request.POST['size']
+        print(color + ' ' + size)
+        
+
     product = Product.objects.get(id=product_id)
     try:
         # get the cart using the cart_id present in session
--- store/models.py
+++ store/models.py
@@ -34,6 +34,14 @@ class Product(models.Model):
         return self.product_name
 
 
+class VariationManager(models.Manager):
+    def colors(self):
+        return super(VariationManager, self).filter(variation_category='color', is_active=True)
+
+    def sizes(self):
+        return super(VariationManager, self).filter(variation_category='size', is_active=True)
+
+
 variation_category_choice = (
     ('color', 'color'),
     ('size', 'size'),
@@ -47,6 +55,8 @@ class Variation(models.Model):
     variation_value = models.CharField(max_length=100)
     is_active = models.BooleanField(default=True)
     created_date = models.DateTimeField(auto_now=True)
+
+    objects = VariationManager()
 
     def __unicode__(self) -> str:
         return self.product
