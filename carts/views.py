fe95db9aa69f234d863e13671cdf482966c06b23
Eduardo Apodaca <eduardo1ja99@gmail.com>
2021-06-11 12:15:15 -0500

    feat: Add variation in cart item

--- carts/views.py
+++ carts/views.py
@@ -16,7 +16,7 @@ def _cart_id(request):
 
 def add_cart(request, product_id):
     product = Product.objects.get(id=product_id)
-    product_variation=[]
+    product_variation = []
     if request.method == 'POST':
         for item in request.POST:
             key = item
@@ -40,6 +40,11 @@ def add_cart(request, product_id):
 
     try:
         cart_item = CartItem.objects.get(product=product, cart=cart)
+        if len(product_variation) > 0:
+            cart_item.variations.clear()
+            for item in product_variation:
+                cart_item.variations.add(item)
+
         cart_item.quantity += 1
         cart_item.save()
     except CartItem.DoesNotExist:
@@ -48,6 +53,10 @@ def add_cart(request, product_id):
             quantity=1,
             cart=cart
         )
+        if len(product_variation) > 0:
+            cart_item.variations.clear()
+            for item in product_variation:
+                cart_item.variations.add(item)
         cart_item.save()
     # return HttpResponse(cart_item.product)
     # exit()
