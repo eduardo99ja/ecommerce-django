38b0a9810754a4f94ac0d0531d102801c3b53368
Apodaca <eduardo1ja99@gmail.com>
2021-07-02 18:06:59 -0500

    feat:Registration  Making View & Editing Model Form Clean Method to Check Passwords

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,4 @@
+from accounts.models import Account
 from accounts.forms import RegistrationForm
 from django.shortcuts import render
 
@@ -5,7 +6,22 @@ from django.shortcuts import render
 
 
 def register(request):
-    form = RegistrationForm()
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            first_name = form.cleaned_data['first_name']
+            last_name = form.cleaned_data['last_name']
+            phone_number = form.cleaned_data['phone_number']
+            email = form.cleaned_data['email']
+            password = form.cleaned_data['password']
+            username = email.split("@")[0]
+
+            user = Account.objects.create_user(
+                first_name=first_name, last_name=last_name, email=email, username=username, password=password)
+            user.phone_number = phone_number
+            user.save()
+    else:
+        form = RegistrationForm()
     context = {
         'form': form
     }
