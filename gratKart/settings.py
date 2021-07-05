e156b6ce726b20651d24756b262671cd5ea13c5e
Apodaca <eduardo1ja99@gmail.com>
2021-07-05 16:14:41 -0500

    feat: Encode user pk and send token based activation link

--- gratKart/settings.py
+++ gratKart/settings.py
@@ -141,3 +141,10 @@ MEDIA_ROOT = BASE_DIR / 'media'
 MESSAGE_TAGS = {
     messages.ERROR: 'danger',
 }
+
+#SMTP CONFIGURATION
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'eduardo1ja99@gmail.com'
+EMAIL_HOST_PASSWORD = ''
+EMAIL_USE_TLS = True
