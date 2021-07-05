454cd0f05c0d2c3755b8aea5f94c3cf2d8ecca5f
Apodaca <eduardo1ja99@gmail.com>
2021-07-05 17:17:23 -0500

    feat: dashboard

--- accounts/views.py
+++ accounts/views.py
@@ -65,8 +65,8 @@ def login(request):
         print(user)
         if user is not None:
             auth.login(request, user)
-            # messages.success(request, 'You are now logged in')
-            return redirect('home')
+            messages.success(request, 'You are now logged in')
+            return redirect('dashboard')
         else:
             messages.error(request, 'Invalid loggin credentials')
             return redirect('login')
@@ -94,3 +94,7 @@ def activate(request, uidb64, token):
     else:
         messages.error(request, 'Invalid activation link')
         return redirect('register')
+
+@login_required(login_url='login')
+def dashboard(request):
+    return render(request,'accounts/dashboard.html')
