5e9b4f969da03a6babb86d4669376257d33781b4
Apodaca <eduardo1ja99@gmail.com>
2021-07-09 16:53:06 -0500

    feat: Assign the user to the cart item

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,4 @@
+from carts.models import Cart, CartItem
 from django.contrib import messages, auth
 from accounts.models import Account
 from accounts.forms import RegistrationForm
@@ -12,6 +13,8 @@ from django.utils.http import urlsafe_base64_decode, urlsafe_base64_encode
 from django.utils.encoding import force_bytes
 from django.contrib.auth.tokens import default_token_generator
 from django.core.mail import EmailMessage
+
+from carts.views import _cart_id
 
 # Create your views here.
 
@@ -62,8 +65,20 @@ def login(request):
         password = request.POST['password']
 
         user = auth.authenticate(email=email, password=password)
-        print(user)
         if user is not None:
+            try:
+                cart = Cart.objects.get(cart_id=_cart_id(request))
+                is_cart_item_exists = CartItem.objects.filter(
+                    cart=cart).exists()
+                if is_cart_item_exists:
+                    cart_item = CartItem.objects.filter(cart=cart)
+
+
+                    for item in cart_item:
+                        item.user = user
+                        item.save()
+            except:
+                pass
             auth.login(request, user)
             messages.success(request, 'You are now logged in')
             return redirect('dashboard')
--- carts/views.py
+++ carts/views.py
@@ -3,6 +3,7 @@ from django.core.exceptions import ObjectDoesNotExist
 from django.http.response import HttpResponse
 from django.shortcuts import get_object_or_404, redirect, render
 from .models import Cart, CartItem, Product
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
@@ -137,6 +138,7 @@ def cart(request, total=0, quantity=0, cart_items=None):
     return render(request, 'store/cart.html', context)
 
 
+@login_required(login_url='login')
 def checkout(request, total=0, quantity=0, cart_items=None):
     try:
         tax = 0
