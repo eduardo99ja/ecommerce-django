42dfe35ed0da5da1c38eac8945d46ade7abb5f9c
Apodaca <eduardo1ja99@gmail.com>
2021-07-12 17:00:17 -0500

    feat: Place order view and generate order number

--- orders/views.py
+++ orders/views.py
@@ -1,12 +1,13 @@
+from orders.models import Order
 from orders.forms import OrderForm
 from django.http.response import HttpResponse
 from django.shortcuts import redirect, render
 from carts.models import CartItem
-
+import datetime
 # Create your views here.
 
 
-def place_order(request):
+def place_order(request, total=0, quantity=0):
     current_user = request.user
 
     # If cart count is less than or equal to 0,then redirec back to shop
@@ -15,5 +16,54 @@ def place_order(request):
     if cart_count <= 0:
         return redirect('store')
 
+    grand_total = 0
+    tax = 0
+    for cart_item in cart_items:
+        total += (cart_item.product.price * cart_item.quantity)
+        quantity += cart_item.quantity
+
+    tax = (2 * total)/100
+    grand_total = total + tax
+
     if request.method == 'POST':
         form = OrderForm(request.POST)
+        if form.is_valid():
+            # store all the billing information inside order table
+            data = Order()
+            data.user = current_user
+            data.first_name = form.cleaned_data['first_name']
+            data.last_name = form.cleaned_data['last_name']
+            data.phone = form.cleaned_data['phone']
+            data.email = form.cleaned_data['email']
+            data.address_line_1 = form.cleaned_data['address_line_1']
+            data.address_line_2 = form.cleaned_data['address_line_2']
+            data.country = form.cleaned_data['country']
+            data.state = form.cleaned_data['state']
+            data.city = form.cleaned_data['city']
+            data.order_note = form.cleaned_data['order_note']
+            data.order_total = grand_total
+            data.tax = tax
+            data.ip = request.META.get('REMOTE_ADDR')
+            data.save()
+            # Generate order number
+            yr = int(datetime.date.today().strftime('%Y'))
+            dt = int(datetime.date.today().strftime('%d'))
+            mt = int(datetime.date.today().strftime('%m'))
+            d = datetime.date(yr, mt, dt)
+            current_date = d.strftime("%Y%m%d")  # 20210305
+            order_number = current_date + str(data.id)
+            data.order_number = order_number
+            data.save()
+
+            order = Order.objects.get(
+                user=current_user, is_ordered=False, order_number=order_number)
+            context = {
+                'order': order,
+                'cart_items': cart_items,
+                'total': total,
+                'tax': tax,
+                'grand_total': grand_total,
+            }
+            return HttpResponse('ok')
+    else:
+        return redirect('checkout')
