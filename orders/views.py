dcfc19675308d342b7e1c7742c1c7d4fe4e2b702
Apodaca <eduardo1ja99@gmail.com>
2021-07-13 16:07:32 -0500

    feat: Review order page setup

--- orders/views.py
+++ orders/views.py
@@ -67,3 +67,7 @@ def place_order(request, total=0, quantity=0):
             return HttpResponse('ok')
     else:
         return redirect('checkout')
+
+
+def payments(request):
+    return render(request, 'orders/payments.html')
