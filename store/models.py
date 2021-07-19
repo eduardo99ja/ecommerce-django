10500ba3637dc6296766e52aaed9cfd49d8fe2ed
Apodaca <eduardo1ja99@gmail.com>
2021-07-19 12:33:18 -0500

    feat: ReviewRating model

--- store/models.py
+++ store/models.py
@@ -1,3 +1,4 @@
+from accounts.models import Account
 from django.db import models
 from django.db.models.deletion import CASCADE
 from category.models import Category
@@ -60,3 +61,18 @@ class Variation(models.Model):
 
     def __str__(self) -> str:
         return self.variation_value
+
+
+class ReviewRating(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    user = models.ForeignKey(Account, on_delete=models.CASCADE)
+    subject = models.CharField(max_length=100, blank=True)
+    review = models.TextField(max_length=500, blank=True)
+    rating = models.FloatField()
+    ip = models.CharField(max_length=20, blank=True)
+    status = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.subject
