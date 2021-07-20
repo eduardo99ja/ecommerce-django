b317cc5428c0a28b8ecffd7e7bde811b63f0c903
Apodaca <eduardo1ja99@gmail.com>
2021-07-20 13:20:28 -0500

    feat: Order detail page

--- accounts/views.py
+++ accounts/views.py
@@ -13,7 +13,7 @@ from django.utils.http import urlsafe_base64_decode, urlsafe_base64_encode
 from django.utils.encoding import force_bytes
 from django.contrib.auth.tokens import default_token_generator
 from django.core.mail import EmailMessage
-from orders.models import Order
+from orders.models import Order, OrderProduct
 import requests
 
 from carts.views import _cart_id
@@ -277,3 +277,19 @@ def change_password(request):
             messages.error(request, 'Password does not match!')
             return redirect('change_password')
     return render(request, 'accounts/change_password.html')
+
+
+@login_required(login_url='login')
+def order_detail(request, order_id):
+    order_detail = OrderProduct.objects.filter(order__order_number=order_id)
+    order = Order.objects.get(order_number=order_id)
+    subtotal = 0
+    for i in order_detail:
+        subtotal += i.product_price * i.quantity
+
+    context = {
+        'order_detail': order_detail,
+        'order': order,
+        'subtotal': subtotal,
+    }
+    return render(request, 'accounts/order_detail.html', context)
